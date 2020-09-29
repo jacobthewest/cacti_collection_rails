aba9a72f6ca023269c61871a56597e47b53eb924
jacobthewest <jacobthewest@gmail.com>
2020-09-29 13:10:11 -0600

    Created the Cactus <--singular-- model, updated the fields common_name, genus, species, and hardiness to be not null, and made the db migration

---
+++ db/migrate/20200929190832_create_cactus.rb
@@ -0,0 +1,23 @@
+class CreateCactus < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cactus do |t|
+      t.string :common_name, null:false
+      t.string :genus, null:false
+      t.string :species, null:false
+      t.string :cultivar
+      t.text :light_requirements
+      t.text :soil_requirements
+      t.text :water_requirements
+      t.text :fruit
+      t.text :flower
+      t.text :hardiness, null:false
+      t.text :fertilizer
+      t.text :propagation
+      t.text :growth_rate
+      t.text :max_size
+      t.text :interesting_facts
+
+      t.timestamps
+    end
+  end
+end
