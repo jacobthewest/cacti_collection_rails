028229498bf57688b39de686cd093f83808c802f
jacobthewest <jacobthewest@gmail.com>
2020-09-29 13:16:55 -0600

    Aded all of the necesary skeleton functions for the CactiController. Also created files with basic test text for the views index and new

--- app/controllers/cacti_controller.rb
+++ app/controllers/cacti_controller.rb
@@ -1,2 +1,25 @@
 class CactiController < ApplicationController
+
+    # Correct order is index, show, new, edit, create, update, destroy
+    
+    def index
+    end
+
+    def show
+    end
+    
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
