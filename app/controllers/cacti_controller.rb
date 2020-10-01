0380622dcaf126ccf27d40abeb0960277ff66630
jacobthewest <jacobthewest@gmail.com>
2020-10-01 12:20:06 -0600

    Basic functionality is woring right now for the entire site.

--- app/controllers/cacti_controller.rb
+++ app/controllers/cacti_controller.rb
@@ -3,23 +3,49 @@ class CactiController < ApplicationController
     # Correct order is index, show, new, edit, create, update, destroy
     
     def index
+        @cacti = Cactus.all
     end
 
     def show
+        @cactus = Cactus.find(params[:id])
     end
     
     def new
+        @cactus = Cactus.new
     end
 
     def edit
+        @cactus = Cactus.find(params[:id])
     end
 
     def create
+        @cactus = Cactus.new(cactus_params)
+       
+        @cactus.save
+        redirect_to @cactus
     end
 
     def update
+        @cactus = Cactus.find(params[:id])
+
+        if @cactus.update(cactus_params)
+            redirect_to @cactus
+        else
+            render 'edit'
+        end
     end
 
     def destroy
+        @cactus = Cactus.find(params[:id])
+        @cactus.destroy
+       
+        render 'index'      
     end
+
+    private
+        def cactus_params
+            params.require(:cactus).permit(:common_name, :genus, :species, :cultivar, :light_requirements, 
+            :soil_requirements, :water_requirements, :fruit, :flower, :hardiness, :fertilizer, :propagation,
+            :growth_rate, :max_size, :interesting_facts)
+        end
 end
--- app/models/cactus.rb
+++ app/models/cactus.rb
@@ -1,2 +1,6 @@
 class Cactus < ApplicationRecord
+    validates :common_name, presence: true
+    validates :genus, presence: true
+    validates :species, presence: true
+    validates :hardiness, presence: true
 end
